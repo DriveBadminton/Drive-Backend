661ba02523e08f62c674898a1c8b7feaf318eb9e
Gumraze-git <galaxydh4110@gmail.com>
2025-09-09 16:07:41 +0900

    initial commit

---
+++ src/test/java/com/gumraze/drive/backend/DriveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gumraze.drive.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
