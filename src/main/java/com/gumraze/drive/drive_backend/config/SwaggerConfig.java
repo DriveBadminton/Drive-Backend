7488badf2bffc3202fa03988fa150d20ee1672ec
gumraze-git <galaxydh4110@gmail.com>
2025-10-02 09:51:16 +0900

    feat: Swagger 설정 추가
    - Swagger 설정 클래스(SwaggerConfig.java) 생성
    - SpringDoc 의존성 추가: `springdoc-openapi-starter-webmvc-ui` версии 2.6.0
    - API 명세 정보 및 기본 인증(SecurityScheme) 구성 추가

---
+++ src/main/java/com/gumraze/drive/drive_backend/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.gumraze.drive.drive_backend.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "basicAuth";
+
+    @Bean
+    public OpenAPI driveBackendOpenAPI() {
+        return new OpenAPI()
+            .info(apiInfo())
+            .components(new Components().addSecuritySchemes(SECURITY_SCHEME_NAME, basicAuthScheme()))
+            .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME));
+    }
+
+    private Info apiInfo() {
+        return new Info()
+            .title("Drive Backend API")
+            .description("동호인 배드민턴 대회/선수 관리 백엔드 API 명세")
+            .version("0.0.1");
+    }
+
+    private SecurityScheme basicAuthScheme() {
+        return new SecurityScheme()
+            .name(SECURITY_SCHEME_NAME)
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("basic");
+    }
+}
