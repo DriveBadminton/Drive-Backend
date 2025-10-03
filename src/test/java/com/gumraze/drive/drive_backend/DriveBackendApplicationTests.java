417f71140b5eec504be6307ecf2281719464a5d2
gumraze-git <galaxydh4110@gmail.com>
2025-10-03 22:19:34 +0900

    chore: 테스트 환경 설정 및 파일 정리
    - H2 데이터베이스 의존성 추가
    - 테스트 프로파일 활성화 애노테이션(@ActiveProfiles("test")) 추가
    - 불필요한 테스트 파일 경로 및 클래스 재구성

--- src/test/java/com/gumraze/drive/drive_backend/DriveBackendApplicationTests.java
+++ src/test/java/com/gumraze/drive/drive_backend/DriveBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.gumraze.drive.drive_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DriveBackendApplicationTests {
 
 	@Test
