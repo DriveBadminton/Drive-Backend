f193b9c53348f23d5e42c5906f43f0a488012d10
gumraze-git <galaxydh4110@gmail.com>
2025-12-14 08:46:07 +0900

    feat(entity): UserAuth 엔티티 클래스 추가
    - 사용자 인증 정보를 저장하는 UserAuth 엔티티 생성
    - 인증 제공자, 이메일, 닉네임, 프로필 이미지 URL 등 필드 정의
    - 사용자와 다대일 관계 설정
    - 생성/수정 시간 필드 추가

---
+++ src/main/java/com/gumraze/drive/drive_backend/user/entity/UserAuth.java
@@ -0,0 +1,30 @@
+package com.gumraze.drive.drive_backend.user.entity;
+
+import com.gumraze.drive.drive_backend.user.constants.AuthProvider;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_auth")
+public class UserAuth {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // User 1 <- N UserAuth(Kakao, Google, ... )
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    private AuthProvider provider;
+    private String providerUserId;
+
+    private String email;
+    private String nickname;
+    private String profileImageUrl;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
