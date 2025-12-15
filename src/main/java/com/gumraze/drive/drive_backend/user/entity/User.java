3cdd3b915a99b1ce1e8cebefe1b707d507a50599
gumraze-git <galaxydh4110@gmail.com>
2025-12-15 10:19:59 +0900

    feat(entity): User 및 UserAuth 엔티티 수정
    - User 엔티티에 기본 생성자 및 생성자 추가
    - UserAuth 엔티티에 uniqueConstraints 설정 추가
    - UserAuth 엔티티에 필드 validation 어노테이션 추가
    - UserAuth 엔티티에 생성자 및 기본 생성자 추가
    - User, UserAuth 엔티티에 Lombok @Getter 어노테이션 적용

--- src/main/java/com/gumraze/drive/drive_backend/user/entity/User.java
+++ src/main/java/com/gumraze/drive/drive_backend/user/entity/User.java
@@ -3,9 +3,11 @@ package com.gumraze.drive.drive_backend.user.entity;
 import com.gumraze.drive.drive_backend.user.constants.UserRole;
 import com.gumraze.drive.drive_backend.user.constants.UserStatus;
 import jakarta.persistence.*;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
+@Getter
 @Entity
 @Table(name = "users")
 public class User {
@@ -27,4 +29,17 @@ public class User {
 
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
+
+    // JPA 프록시 생성을 위한 기본 생성자
+    protected User() {}
+
+    public User(
+            UserStatus status,
+            UserRole role
+    ) {
+        this.status = status;
+        this.role = role;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
 }
--- src/main/java/com/gumraze/drive/drive_backend/user/entity/UserAuth.java
+++ src/main/java/com/gumraze/drive/drive_backend/user/entity/UserAuth.java
@@ -2,11 +2,18 @@ package com.gumraze.drive.drive_backend.user.entity;
 
 import com.gumraze.drive.drive_backend.auth.constants.AuthProvider;
 import jakarta.persistence.*;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "user_auth")
+@Getter
+@Table(
+    name = "user_auth",
+    uniqueConstraints = {
+            @UniqueConstraint(columnNames = {"provider", "provider_user_id"})
+    }
+)
 public class UserAuth {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,17 +21,39 @@ public class UserAuth {
 
     // User 1 <- N UserAuth(Kakao, Google, ... )
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private AuthProvider provider;
+
+    @Column(name = "provider_user_id", nullable = false)
     private String providerUserId;
 
     private String email;
     private String nickname;
     private String profileImageUrl;
 
+    @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
+
+    // JPA 기본 생성자
+    protected UserAuth() {}
+
+    // OAuth 로그인 시 사용하는 최소 생성자
+    public UserAuth(
+        User user,
+        AuthProvider provider,
+        String providerUserId
+    ) {
+        this.user = user;
+        this.provider = provider;
+        this.providerUserId = providerUserId;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
 }
