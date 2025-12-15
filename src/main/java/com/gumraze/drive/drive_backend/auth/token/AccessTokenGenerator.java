e5bae5d28511696606785b336fa7c83beeb5f64a
gumraze-git <galaxydh4110@gmail.com>
2025-12-15 11:19:08 +0900

    feat(auth): AccessTokenGenerator 클래스에 @Component 어노테이션 추가
    - AccessTokenGenerator를 Spring Bean으로 등록하여 의존성 주입 가능하도록 변경

--- src/main/java/com/gumraze/drive/drive_backend/auth/token/AccessTokenGenerator.java
+++ src/main/java/com/gumraze/drive/drive_backend/auth/token/AccessTokenGenerator.java
@@ -1,7 +1,10 @@
 package com.gumraze.drive.drive_backend.auth.token;
 
+import org.springframework.stereotype.Component;
+
 import java.util.UUID;
 
+@Component
 public class AccessTokenGenerator implements TokenProvider{
     @Override
     public String generateAccessToken(Long userId) {
