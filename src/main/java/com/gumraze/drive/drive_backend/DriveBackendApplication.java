2e35beca7374017f1c8d444ef9c048cf2d30b865
gumraze-git <galaxydh4110@gmail.com>
2025-12-16 15:46:24 +0900

    feat(core): ConfigurationPropertiesScan 활성화
    - @ConfigurationPropertiesScan 추가를 통해 설정 프로퍼티 자동 스캔 기능 활성화 및 향후 설정 관리 간소화

--- src/main/java/com/gumraze/drive/drive_backend/DriveBackendApplication.java
+++ src/main/java/com/gumraze/drive/drive_backend/DriveBackendApplication.java
@@ -2,8 +2,10 @@ package com.gumraze.drive.drive_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class DriveBackendApplication {
 
     public static void main(String[] args) {
