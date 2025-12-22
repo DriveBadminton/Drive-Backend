09eb2300aea6fba50fa4a5fed485b5c8359c6542
gumraze-git <galaxydh4110@gmail.com>
2025-12-22 10:22:05 +0900

    refac(user): JpaUserAuthRepository 메서드 불필요한 코드 제거
    - 사용되지 않는 user 메서드 제거하여 불필요한 코드 정리

--- src/main/java/com/gumraze/drive/drive_backend/user/repository/JpaUserAuthRepository.java
+++ src/main/java/com/gumraze/drive/drive_backend/user/repository/JpaUserAuthRepository.java
@@ -1,7 +1,6 @@
 package com.gumraze.drive.drive_backend.user.repository;
 
 import com.gumraze.drive.drive_backend.auth.constants.AuthProvider;
-import com.gumraze.drive.drive_backend.user.entity.User;
 import com.gumraze.drive.drive_backend.user.entity.UserAuth;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -16,6 +15,4 @@ public interface JpaUserAuthRepository extends JpaRepository<UserAuth, Long> {
 
     // 사용자의 인증 정보들 중에서 가장 최근에 업데이트된 데이터를 하나 찾는 메서드
     Optional<UserAuth> findFirstByUser_IdOrderByUpdatedAtDesc(Long userId);
-
-    Long user(User user);
 }
