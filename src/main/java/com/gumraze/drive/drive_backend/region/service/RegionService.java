691f21b72394e77c06eb7ed9fcdb1ff369f4821a
gumraze-git <galaxydh4110@gmail.com>
2025-12-25 13:36:26 +0900

    feat(region): RegionService 인터페이스에 시/도 및 구/군 조회 메서드 추가
    - 시/도 지역 정보를 조회하는 getProvinces 메서드 추가
    - 특정 시/도의 구/군 정보를 조회하는 getDistricts 메서드 추가

--- src/main/java/com/gumraze/drive/drive_backend/region/service/RegionService.java
+++ src/main/java/com/gumraze/drive/drive_backend/region/service/RegionService.java
@@ -1,6 +1,9 @@
 package com.gumraze.drive.drive_backend.region.service;
 
 import com.gumraze.drive.drive_backend.region.entity.RegionDistrict;
+import com.gumraze.drive.drive_backend.region.entity.RegionProvince;
+
+import java.util.List;
 
 public interface RegionService {
 
@@ -9,5 +12,10 @@ public interface RegionService {
 
     // 레퍼런스를 ID로 조회
     RegionDistrict getReferenceById(Long district_id);
+
+    // 시/도 지역 정보 조회
+    List<RegionProvince> getProvinces();
+
+    List<RegionDistrict> getDistricts(Long province_id);
 
 }
