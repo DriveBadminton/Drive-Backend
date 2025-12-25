5152097fd46d480f57be718074d74b1feea3f207
gumraze-git <galaxydh4110@gmail.com>
2025-12-25 13:36:21 +0900

    feat(region): 시/도 및 구/군 조회 로직 추가
    - 시/도를 ID 오름차순으로 조회하는 getProvinces 메서드 추가
    - 특정 시/도의 구/군을 ID 오름차순으로 조회하는 getDistricts 메서드 추가
    - JpaRegionDistrictRepository에 province ID로 구/군 조회를 위한 findAllByProvinceId 메서드 추가

--- src/main/java/com/gumraze/drive/drive_backend/region/repository/JpaRegionDistrictRepository.java
+++ src/main/java/com/gumraze/drive/drive_backend/region/repository/JpaRegionDistrictRepository.java
@@ -4,6 +4,10 @@ import com.gumraze.drive.drive_backend.region.entity.RegionDistrict;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface JpaRegionDistrictRepository extends JpaRepository<RegionDistrict, Long> {
+    // province Id로 district 조회
+    List<RegionDistrict> findAllByProvinceId(Long province_id);
 }
--- src/main/java/com/gumraze/drive/drive_backend/region/service/RegionServiceImpl.java
+++ src/main/java/com/gumraze/drive/drive_backend/region/service/RegionServiceImpl.java
@@ -1,15 +1,21 @@
 package com.gumraze.drive.drive_backend.region.service;
 
 import com.gumraze.drive.drive_backend.region.entity.RegionDistrict;
+import com.gumraze.drive.drive_backend.region.entity.RegionProvince;
 import com.gumraze.drive.drive_backend.region.repository.JpaRegionDistrictRepository;
+import com.gumraze.drive.drive_backend.region.repository.JpaRegionProvinceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class RegionServiceImpl implements RegionService {
 
     private final JpaRegionDistrictRepository jpaRegionDistrictRepository;
+    private final JpaRegionProvinceRepository jpaRegionProvinceRepository;
 
     @Override
     public boolean existsById(Long district_id) {
@@ -19,5 +25,19 @@ public class RegionServiceImpl implements RegionService {
     @Override
     public RegionDistrict getReferenceById(Long district_id) {
         return jpaRegionDistrictRepository.getReferenceById(district_id);
+    }
+
+    @Override
+    public List<RegionProvince> getProvinces() {
+        return jpaRegionProvinceRepository.findAll().stream()
+                .sorted(Comparator.comparing(RegionProvince::getId))
+                .toList();
+    }
+
+    @Override
+    public List<RegionDistrict> getDistricts(Long province_id) {
+        return jpaRegionDistrictRepository.findAllByProvinceId(province_id).stream()
+                .sorted(Comparator.comparing(RegionDistrict::getId))
+                .toList();
     }
 }
