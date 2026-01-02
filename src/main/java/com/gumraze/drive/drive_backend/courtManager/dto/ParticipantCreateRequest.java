9b77a4bdbc46164acf0ae91257f236dfe380a60f
gumraze-git <galaxydh4110@gmail.com>
2026-01-02 21:35:27 +0900

    feat(courtManager): ParticipantCreateRequest 빌더 추가
    - Lombok @Builder 어노테이션 추가
    - @AllArgsConstructor 어노테이션 적용

--- src/main/java/com/gumraze/drive/drive_backend/courtManager/dto/ParticipantCreateRequest.java
+++ src/main/java/com/gumraze/drive/drive_backend/courtManager/dto/ParticipantCreateRequest.java
@@ -4,13 +4,13 @@ import com.gumraze.drive.drive_backend.user.constants.Gender;
 import com.gumraze.drive.drive_backend.user.constants.Grade;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ParticipantCreateRequest {
     private Long userId; // 우리 서비스의 사용자인 경우에만 수집
 
