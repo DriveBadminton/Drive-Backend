3ccd8d7c5ee324f4b2a58db9419c1488c0d3c8d7
gumraze-git <galaxydh4110@gmail.com>
2026-01-02 20:21:10 +0900

    refactor(courtManager): FreeGameRepository 삭제 및 CourtGameRepository 추가
    - FreeGameRepository 인터페이스 제거
    - CourtGameRepository 인터페이스 새로 생성하여 CourtGame 엔티티 처리

--- src/main/java/com/gumraze/drive/drive_backend/courtManager/repository/FreeGameRepository.java
+++
@@ -1,6 +0,0 @@
-package com.gumraze.drive.drive_backend.courtManager.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FreeGameRepository extends JpaRepository<FreeGame, Long> {
-}
