e72aff6bd3e2bcd6f68b8cd1a7a9c55e8f14e3ad
gumraze-git <galaxydh4110@gmail.com>
2026-01-02 21:35:09 +0900

    feat(courtManager): 자유게임 응답 DTO 변환 로직 추가
    - CourtGame 엔티티를 CreateFreeGameResponse로 변환하는 정적 메서드 추가

--- src/main/java/com/gumraze/drive/drive_backend/courtManager/dto/CreateFreeGameResponse.java
+++ src/main/java/com/gumraze/drive/drive_backend/courtManager/dto/CreateFreeGameResponse.java
@@ -1,5 +1,6 @@
 package com.gumraze.drive.drive_backend.courtManager.dto;
 
+import com.gumraze.drive.drive_backend.courtManager.entity.CourtGame;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,4 +8,10 @@ import lombok.Setter;
 @Setter
 public class CreateFreeGameResponse {
     private Long gameId;
+
+    public static CreateFreeGameResponse from(CourtGame courtGame) {
+        CreateFreeGameResponse response = new CreateFreeGameResponse();
+        response.setGameId(courtGame.getId());
+        return response;
+    }
 }
