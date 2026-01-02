03e2064bbdf1aed9e9270802872a3f6450688db9
gumraze-git <galaxydh4110@gmail.com>
2026-01-02 20:17:10 +0900

    feat(courtManager): FreeGameService 인터페이스 및 구현 클래스 추가
    - FreeGameService 인터페이스 생성
    - FreeGameServiceImpl 구현 클래스에서 createFreeGame 메서드 정의

---
+++ src/main/java/com/gumraze/drive/drive_backend/courtManager/service/FreeGameServiceImpl.java
@@ -0,0 +1,12 @@
+package com.gumraze.drive.drive_backend.courtManager.service;
+
+import com.gumraze.drive.drive_backend.courtManager.dto.CreateFreeGameRequest;
+import com.gumraze.drive.drive_backend.courtManager.dto.CreateFreeGameResponse;
+
+public class FreeGameServiceImpl implements FreeGameService {
+
+    @Override
+    public CreateFreeGameResponse createFreeGame(CreateFreeGameRequest request) {
+        return null;
+    }
+}
