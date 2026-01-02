fb34949e7fc6bd3a06a8b6ef69e36e027c08ebdd
gumraze-git <galaxydh4110@gmail.com>
2026-01-02 18:18:37 +0900

    chore: CourtTournament 관련 코드 전면 제거
    - 새로운 기능 개발을 위해, 프로젝트 개편 이전 코드 삭제

--- src/main/java/com/gumraze/drive/drive_backend/courtmanager/repository/CourtTournamentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gumraze.drive.drive_backend.courtmanager.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gumraze.drive.drive_backend.courtmanager.entity.CourtTournament;
-
-public interface CourtTournamentRepository extends JpaRepository<CourtTournament, Long> {
-}
