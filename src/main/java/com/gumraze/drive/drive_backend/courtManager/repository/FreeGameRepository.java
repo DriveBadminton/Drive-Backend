ff954d1c7aee8ff78e1b8d8fb0af5775e0c47c88
gumraze-git <galaxydh4110@gmail.com>
2026-01-02 20:17:17 +0900

    feat(courtManager): FreeGameRepository 인터페이스 추가
    - FreeGame 엔티티를 처리하기 위한 JpaRepository 인터페이스 생성

---
+++ src/main/java/com/gumraze/drive/drive_backend/courtManager/repository/FreeGameRepository.java
@@ -0,0 +1,6 @@
+package com.gumraze.drive.drive_backend.courtManager.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FreeGameRepository extends JpaRepository<FreeGame, Long> {
+}
