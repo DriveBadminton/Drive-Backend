94a7e199c4ecf58acc086e8442517bc32ee7e6d4
gumraze-git <galaxydh4110@gmail.com>
2026-01-02 21:35:16 +0900

    feat(courtManager): 자유게임 생성 로직 추가
    - CourtGame 엔티티 생성 및 저장 로직 구현
    - CreateFreeGameResponse 생성 및 반환

--- src/main/java/com/gumraze/drive/drive_backend/courtManager/service/FreeGameServiceImpl.java
+++ src/main/java/com/gumraze/drive/drive_backend/courtManager/service/FreeGameServiceImpl.java
@@ -1,8 +1,13 @@
 package com.gumraze.drive.drive_backend.courtManager.service;
 
+import com.gumraze.drive.drive_backend.courtManager.constants.GameStatus;
+import com.gumraze.drive.drive_backend.courtManager.constants.GameType;
 import com.gumraze.drive.drive_backend.courtManager.dto.CreateFreeGameRequest;
 import com.gumraze.drive.drive_backend.courtManager.dto.CreateFreeGameResponse;
+import com.gumraze.drive.drive_backend.courtManager.entity.CourtGame;
 import com.gumraze.drive.drive_backend.courtManager.repository.CourtGameRepository;
+
+import java.time.LocalDateTime;
 
 public class FreeGameServiceImpl implements FreeGameService {
 
@@ -14,6 +19,22 @@ public class FreeGameServiceImpl implements FreeGameService {
 
     @Override
     public CreateFreeGameResponse createFreeGame(CreateFreeGameRequest request) {
-        return null;
+        // 엔티티 생성
+        CourtGame courtGame = new CourtGame(
+                null,                       // id는 DB에서 생성
+                request.getTitle(),
+                null,                  // organizer 미구현
+                GameType.FREE,
+                GameStatus.NOT_STARTED,
+                request.getMatchRecordMode(),
+                null,                   // share code 미구현
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+
+        // 저장
+        CourtGame savedGame = courtGameRepository.save(courtGame);
+
+        return CreateFreeGameResponse.from(savedGame);
     }
 }
