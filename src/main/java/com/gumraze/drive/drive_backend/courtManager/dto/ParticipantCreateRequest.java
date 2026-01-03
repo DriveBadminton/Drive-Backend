12812e23c16df3e27c6535893eaacb9c792e83fc
gumraze-git <galaxydh4110@gmail.com>
2026-01-03 19:29:18 +0900

    refactor(courtManager): ParticipantCreateRequest 필드 수정
    - 참가자 ID 필드 추가 및 @Id 어노테이션 적용
    - 필드별 주석 보완 및 @NotNull 어노테이션 추가

--- src/main/java/com/gumraze/drive/drive_backend/courtManager/dto/ParticipantCreateRequest.java
+++ src/main/java/com/gumraze/drive/drive_backend/courtManager/dto/ParticipantCreateRequest.java
@@ -2,7 +2,9 @@ package com.gumraze.drive.drive_backend.courtManager.dto;
 
 import com.gumraze.drive.drive_backend.user.constants.Gender;
 import com.gumraze.drive.drive_backend.user.constants.Grade;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
@@ -12,9 +14,13 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class ParticipantCreateRequest {
-    private Long userId; // 우리 서비스의 사용자인 경우에만 수집
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;                // 참가자 ID, 각 참가자의 구분을 위한 Id
 
-    @NotBlank
+    private Long userId;            // 우리 서비스의 사용자인 경우에만 수집
+
+    @NotNull
     private String name;            // 참가자 이름
 
     @NotNull
@@ -23,5 +29,6 @@ public class ParticipantCreateRequest {
     @NotNull
     private Grade grade;            // 참가자 급수
 
-    private Integer ageGroup;       // (선택) 연령대
+    @NotNull
+    private Integer ageGroup;       // 연령대
 }
