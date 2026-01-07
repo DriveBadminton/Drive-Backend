9ab52de21897fed0a2b7d98dc048bf4500d1b9c0
gumraze-git <galaxydh4110@gmail.com>
2026-01-07 16:21:02 +0900

    refactor(courtManager): FreeGameService에서 @Service 제거
    - 인터페이스에 불필요한 @Service 어노테이션 삭제

--- src/main/java/com/gumraze/drive/drive_backend/courtManager/service/FreeGameService.java
+++ src/main/java/com/gumraze/drive/drive_backend/courtManager/service/FreeGameService.java
@@ -2,9 +2,7 @@ package com.gumraze.drive.drive_backend.courtManager.service;
 
 import com.gumraze.drive.drive_backend.courtManager.dto.CreateFreeGameRequest;
 import com.gumraze.drive.drive_backend.courtManager.dto.CreateFreeGameResponse;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface FreeGameService {
     CreateFreeGameResponse createFreeGame(Long userId, CreateFreeGameRequest request);
 }
