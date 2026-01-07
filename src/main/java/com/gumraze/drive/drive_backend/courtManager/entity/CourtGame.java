445ab6b48faa1f7ba13bea4de364fe62e2aea2c2
gumraze-git <galaxydh4110@gmail.com>
2026-01-07 15:35:12 +0900

    refactor(courtManager): CourtGame organizer 필드 수정
    - organizerId 필드를 User 참조로 변경
    - FreeGameServiceImpl 관련 로직 수정

--- src/main/java/com/gumraze/drive/drive_backend/courtManager/entity/CourtGame.java
+++ src/main/java/com/gumraze/drive/drive_backend/courtManager/entity/CourtGame.java
@@ -4,6 +4,7 @@ import com.gumraze.drive.drive_backend.courtManager.constants.GameStatus;
 import com.gumraze.drive.drive_backend.courtManager.constants.GameType;
 import com.gumraze.drive.drive_backend.courtManager.constants.MatchRecordMode;
 import com.gumraze.drive.drive_backend.user.constants.GradeType;
+import com.gumraze.drive.drive_backend.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,9 +25,9 @@ public class CourtGame {
     @Column(nullable = false)
     private String title;
 
-
-    @Column(name = "organizer_id", nullable = false)
-    private Long organizerId;     // 게임을 생성한 유저 id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "organizer_id", nullable = false)
+    private User organizer;     // 게임을 생성한 유저(FK)
 
     @Enumerated(EnumType.STRING)
     @Column(name = "grade_type", nullable = false)
