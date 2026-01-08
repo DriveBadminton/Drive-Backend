5058e75ee7fed5ec71c03009ba4d53e65f291c1b
gumraze-git <galaxydh4110@gmail.com>
2026-01-08 23:59:11 +0900

    feat(courtManager): 자유 경기 상세 조회 메서드 추가
    - FreeGameService에 getFreeGameDetail 메서드 선언

--- src/main/java/com/gumraze/drive/drive_backend/courtManager/service/FreeGameService.java
+++ src/main/java/com/gumraze/drive/drive_backend/courtManager/service/FreeGameService.java
@@ -2,7 +2,10 @@ package com.gumraze.drive.drive_backend.courtManager.service;
 
 import com.gumraze.drive.drive_backend.courtManager.dto.CreateFreeGameRequest;
 import com.gumraze.drive.drive_backend.courtManager.dto.CreateFreeGameResponse;
+import com.gumraze.drive.drive_backend.courtManager.dto.FreeGameDetailResponse;
 
 public interface FreeGameService {
     CreateFreeGameResponse createFreeGame(Long userId, CreateFreeGameRequest request);
+
+    FreeGameDetailResponse getFreeGameDetail(Long gameId);
 }
