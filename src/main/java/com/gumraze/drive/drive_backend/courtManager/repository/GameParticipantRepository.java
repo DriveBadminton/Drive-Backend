ec8b9bccccbc83e412a106f3b819afced32badf4
gumraze-git <galaxydh4110@gmail.com>
2026-01-11 11:00:20 +0900

    feat(courtManager): 자유게임 repository 추가
    - FreeGameRoundRepository, GameParticipantRepository, FreeGameMatchRepository 생성 및 조회 메서드 추가

--- src/main/java/com/gumraze/drive/drive_backend/courtManager/repository/GameParticipantRepository.java
+++ src/main/java/com/gumraze/drive/drive_backend/courtManager/repository/GameParticipantRepository.java
@@ -3,5 +3,8 @@ package com.gumraze.drive.drive_backend.courtManager.repository;
 import com.gumraze.drive.drive_backend.courtManager.entity.GameParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GameParticipantRepository extends JpaRepository<GameParticipant, Long> {
+    List<GameParticipant> findByGameId(Long gameId);
 }
