c4ba88f029f13980c317bc9f4403b9edc2bdbee4
gumraze-git <galaxydh4110@gmail.com>
2026-01-14 23:26:04 +0900

    test(user): getUserMe 테스트 코드 추가
    - 사용자 상태에 따른 getUserMe 테스트 케이스 구현
    - 사용자 없을 때 예외 발생 테스트 추가

--- src/main/java/com/gumraze/drive/drive_backend/user/service/UserServiceImpl.java
+++ src/main/java/com/gumraze/drive/drive_backend/user/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.gumraze.drive.drive_backend.user.constants.UserStatus;
 import com.gumraze.drive.drive_backend.user.dto.UserMeResponse;
 import com.gumraze.drive.drive_backend.user.entity.User;
 import com.gumraze.drive.drive_backend.user.entity.UserProfile;
+import com.gumraze.drive.drive_backend.user.repository.UserProfileRepository;
 import com.gumraze.drive.drive_backend.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,8 @@ import java.util.Optional;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final UserService userService;
+    private final UserProfileRepository userProfileRepository;
 
     @Override
     public Optional<User> findById(Long userId) {
@@ -23,18 +26,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserMeResponse getUserMe(Long userId) {
-
-        // 사용자 조회, 없으면 실패 처리
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
 
-        // 프로필 조회, 없으면 null 반환
         UserProfile profile = null;
         if (user.getStatus() == UserStatus.ACTIVE) {
             profile = userProfileRepository.findByUserId(userId).orElse(null);
         }
 
-        // 사용자 상태 + 프로필 정보가 있으면 DTO로 반환
         return UserMeResponse.from(user, profile);
     }
 }
