e3c02095b9b67196a9c693b70710024ae3259ec4
gumraze-git <galaxydh4110@gmail.com>
2026-01-14 22:31:35 +0900

    refactor(user): UserRepository 불필요 메서드 제거
    - createUser, findById, existsById 메서드 삭제

--- src/main/java/com/gumraze/drive/drive_backend/user/repository/UserRepository.java
+++ src/main/java/com/gumraze/drive/drive_backend/user/repository/UserRepository.java
@@ -3,13 +3,5 @@ package com.gumraze.drive.drive_backend.user.repository;
 import com.gumraze.drive.drive_backend.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    Long createUser();
-
-    Optional<User> findById(Long id);
-
-    boolean existsById(Long userId);
 }
