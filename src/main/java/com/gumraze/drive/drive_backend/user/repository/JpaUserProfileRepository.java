74f804900b46c78ac5752755db59273583a0cc72
gumraze-git <galaxydh4110@gmail.com>
2026-01-14 16:43:36 +0900

    refactor(user): UserProfileRepository 리팩토링 및 Jpa 통합
    - JpaUserProfileRepository 삭제 및 통합
    - UserProfileRepository 인터페이스 JpaRepository로 확장
    - 불필요한 구현체(UserProfileRepositoryImpl) 제거

--- src/main/java/com/gumraze/drive/drive_backend/user/repository/JpaUserProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gumraze.drive.drive_backend.user.repository;
-
-import com.gumraze.drive.drive_backend.user.entity.UserProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// JPA Adapter
-public interface JpaUserProfileRepository extends JpaRepository<UserProfile, Long> {
-}
-
