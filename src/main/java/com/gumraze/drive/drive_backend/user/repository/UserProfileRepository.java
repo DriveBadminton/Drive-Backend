8c24e4a9dd19c68e83215f98643a7bec4b9f5d40
gumraze-git <galaxydh4110@gmail.com>
2026-01-15 17:27:36 +0900

    refactor(user): 닉네임 검색 메서드에 페이지네이션 추가
    - 닉네임 검색 반환 타입을 List에서 Page로 변경

--- src/main/java/com/gumraze/drive/drive_backend/user/repository/UserProfileRepository.java
+++ src/main/java/com/gumraze/drive/drive_backend/user/repository/UserProfileRepository.java
@@ -1,15 +1,16 @@
 package com.gumraze.drive.drive_backend.user.repository;
 
 import com.gumraze.drive.drive_backend.user.entity.UserProfile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
     Optional<UserProfile> findByUserId(Long userId);
 
-    List<UserProfile> findByNicknameContaining(String nickname);
+    Page<UserProfile> findByNicknameContaining(String nickname, Pageable pageable);
 
     Optional<UserProfile> findByNicknameAndTag(String nickname, String tag);
 }
