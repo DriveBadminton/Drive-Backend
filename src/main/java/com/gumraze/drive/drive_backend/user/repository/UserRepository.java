c943cceb164d0886228652ec2f869238f82d0390
gumraze-git <galaxydh4110@gmail.com>
2026-01-15 10:11:02 +0900

    refactor(user): UserRepository 엔티티 반환 타입 변경
    - UserProfile에서 User로 변경하여 엔티티 일관성 유지

--- src/main/java/com/gumraze/drive/drive_backend/user/repository/UserRepository.java
+++ src/main/java/com/gumraze/drive/drive_backend/user/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.gumraze.drive.drive_backend.user.repository;
 
-import com.gumraze.drive.drive_backend.user.entity.UserProfile;
+import com.gumraze.drive.drive_backend.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserProfile, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
