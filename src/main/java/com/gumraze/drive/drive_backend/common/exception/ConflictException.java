b0d57d515ecfd654b93968854e55a29830460ad7
gumraze-git <galaxydh4110@gmail.com>
2026-01-18 21:14:30 +0900

    feat(common): 커스텀 예외 클래스 추가
    - ConflictException, UnprocessableEntityException 구현

---
+++ src/main/java/com/gumraze/drive/drive_backend/common/exception/ConflictException.java
@@ -0,0 +1,7 @@
+package com.gumraze.drive.drive_backend.common.exception;
+
+public class ConflictException extends RuntimeException {
+    public ConflictException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/gumraze/drive/drive_backend/common/exception/UnprocessableEntityException.java
@@ -0,0 +1,7 @@
+package com.gumraze.drive.drive_backend.common.exception;
+
+public class UnprocessableEntityException extends RuntimeException {
+    public UnprocessableEntityException(String message) {
+        super(message);
+    }
+}
