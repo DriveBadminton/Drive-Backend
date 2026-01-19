ac009ba28c0a2e7e3d84f97d63a13a48aa276e98
gumraze-git <galaxydh4110@gmail.com>
2026-01-19 09:30:47 +0900

    refactor(auth): OAuthUserInfo 클래스로 변경 및 롬복 추가
    - record 대신 class로 변경
    - Getter, Builder, AllArgsConstructor, RequiredArgsConstructor 추가

--- src/main/java/com/gumraze/drive/drive_backend/auth/oauth/OAuthUserInfo.java
+++ src/main/java/com/gumraze/drive/drive_backend/auth/oauth/OAuthUserInfo.java
@@ -1,18 +1,26 @@
 package com.gumraze.drive.drive_backend.auth.oauth;
 
 import com.gumraze.drive.drive_backend.user.constants.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 // provider에 관계없이 동일한 형태로 서비스 계층에 전달하기 위한 표준 OAuth 레코드
-public record OAuthUserInfo(
-        String providerUserId,
-        String email,
-        String nickname,
-        String profileImageUrl,
-        String thumbnailImageUrl,
-        Gender gender,
-        String ageRange,
-        String birthday,
-        boolean emailVerified,
-        boolean phoneNumberVerified
-) {
+
+@Getter
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class OAuthUserInfo {
+    String providerUserId;
+    String email;
+    String nickname;
+    String profileImageUrl;
+    String thumbnailImageUrl;
+    Gender gender;
+    String ageRange;
+    String birthday;
+    Boolean emailVerified;
+    Boolean phoneNumberVerified;
 }
